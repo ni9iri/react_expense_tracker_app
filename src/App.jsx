c954174ca8ab232774e5b81c2ea3f84005be5709
Quyen Nguyen <quyen.ng1902@gmail.com>
2023-06-29 10:54:10 +0300

    organizing component files

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Expenses from "./components/Expenses";
+import Expenses from "./components/Expenses/Expenses";
 
 function App() {
   const expenses = [
@@ -16,7 +16,6 @@ function App() {
 
   return (
     <>
-      <h1>Hello</h1>
       <div className="expenses">
         <Expenses items={expenses} />
       </div>
---
+++ src/components/Expenses/Expenses.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+import Card from "../UI/Card";
+
+function Expenses(props) {
+  return (
+    <Card className="expenses">
+      <ExpenseItem
+        date={props.items[0].date}
+        title={props.items[0].title}
+        amount={props.items[0].amount}
+      />
+      <ExpenseItem
+        date={props.items[1].date}
+        title={props.items[1].title}
+        amount={props.items[1].amount}
+      />
+    </Card>
+  );
+}
+
+export default Expenses;
